39a9e40de6715c60975a9d5707bce476564e7081
Emil Colina <cybre333@gmail.com>
2022-02-15 22:51:01 -0400

    solucion de pratica, condicones simples

--- Js/fizz.Buzz.js
+++ Js/fizz.Buzz.js
@@ -1,2 +1,35 @@
 // alert("conectado");
-document.write();
+
+var divisible = false;
+
+function write_FizzBuzz(ini, end)
+{
+    for(var i = ini; i <= end; i++)
+    {
+        
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            document.write("fizzBuzz");
+        }
+
+        else if (i % 3 == 0)
+        {
+            document.write("fizz");
+            divisible = true;    
+        }
+
+        else if (i % 5 == 0)
+        {
+            document.write("Buzz");    
+            divisible = true
+        }
+        else
+        {
+            document.write(i);
+        }
+        document.write("<br/>");
+    }
+    
+}
+
+write_FizzBuzz(0, 15);
